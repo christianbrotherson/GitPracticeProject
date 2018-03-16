c2024aa18d7c6827eb6817a0c36203f19f410ebe
Christian Brotherson <christian.brotherson@gmail.com>
2018-03-16 10:45:46 -0600

    New feature

---
+++ great_feature.py
@@ -0,0 +1,10 @@
+print("Awesome feature you have there. Been asked to build, I have.")
+
+def crazy():
+    print("Hello worlds")
+
+def feature_component():
+    print("Cool feature bro")
+
+def frame_component():
+    print("I got some slick frames")
