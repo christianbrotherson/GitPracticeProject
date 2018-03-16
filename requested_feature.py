0fb38e115f4391779443ecf7860eb491baf6a3cf
Andrew <goldsabre@gmail.com>
2018-03-16 10:29:26 -0600

    built out the requested feature like a boss

---
+++ requested_feature.py
@@ -0,0 +1,1 @@
+print("Here is your awesome new feature!")
--- second_file.py
+++ second_file.py
@@ -1,2 +1,8 @@
 def another_greeting():
     print("More great code")
+
+def another_greeting():
+    print("This amazing job i did")
+
+def another_greeting():
+    print("Some not so great code")
